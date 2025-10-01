0aaf2eb734227a73b34ad9ba7cc8778f48dfccb9
Zhenya14 <118616573+Zhenya14@users.noreply.github.com>
2025-10-01 15:35:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -888,7 +888,7 @@ document.getElementById("slidernsfw").style.backgroundColor = "gray";
             if (NSFW) NSFW.style.display = "none";
             if (nsfwInfo) nsfwInfo.style.display = "block";
 } else if (age < 16) {
-document.getElementById("private-checkbox{$videoKey}").style.display = "none";
+document.getElementById(`private-checkbox-${videoKey}`).style.display = "none";
           } else {
 document.getElementById("slidernsfw").style.backgroundColor = "red";
             nsfwCheckbox.disabled = false;
