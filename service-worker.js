dfbba7ba9387e1e961e6ad193e80f57fe718bef7
Zhenya14 <118616573+Zhenya14@users.noreply.github.com>
2025-11-06 12:25:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,4 @@
-// ==========================
-// Firebase v8
-// ==========================
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBkPYP3bnDy61NFjRSboRZrfTVNTdIMWbY",
-  authDomain: "videovortex-235cd.firebaseapp.com",
-  databaseURL: "https://videovortex-235cd-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "videovortex-235cd",
-  storageBucket: "videovortex-235cd.appspot.com",
-  messagingSenderId: "681594250269",
-  appId: "1:681594250269:web:1176b21fcc8fe2a7d052f4"
-});
-
-const messaging = firebase.messaging();
-
-// ==========================
-// Фонові push повідомлення
-// ==========================
-messaging.setBackgroundMessageHandler(function (payload) {
-  console.log('[SW] Фонове повідомлення:', payload);
-  const { title, body, icon } = payload.notification;
-  return self.registration.showNotification(title, {
-    body,
-    icon: icon || "/VideoVortex_logo.ico",
-  });
-});
-
-// ==========================
-// Кешування сайту
-// ==========================
 const CACHE_NAME = "videovortex-cache-v1";
 const urlsToCache = [
   "/",
