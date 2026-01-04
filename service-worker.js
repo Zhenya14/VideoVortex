e756f3ff304d2bd8f62b0fa71437c93bc9ccf72e
Zhenya14 <118616573+Zhenya14@users.noreply.github.com>
2026-01-04 13:25:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
-
 const CACHE_NAME = "videovortex-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/script.js",
-  "/VideoVortex_logo_192x192.png",
-  "/VideoVortex_logo_512x512.png"
-];
 
-// Встановлення SW та кешування
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ])
+    )
   );
 });
 
-// Обробка запитів
+self.addEventListener("activate", event => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener("fetch", event => {
-  // Не кешуємо відео
-  if (event.request.url.endsWith(".mp4")) return;
+  if (event.request.destination === "video") return;
 
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(r => r || fetch(event.request))
   );
 });
